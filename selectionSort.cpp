287ca9cec66439576de66dc4e0730c0f6345978d
Curious019 <abdullah.sarder019@gmail.com>
2026-07-21 14:11:12 +0000

    Sorting , take the 1st element as temp_min, then find real min in the unsorted array,and save idx of minElement , then swap with the temp_min

---
+++ selectionSort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n-1;i++){
+        int min_idx=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_idx]){
+                min_idx=j;
+            }
+        }
+            if(min_idx!=i){
+                swap(arr[min_idx],arr[i]);      
+            }
+    }  
+
+}
+
+int main(){
+
+    int n;scanf("%d",&n);
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    selectionSort(arr);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+return 0;
+
+}
